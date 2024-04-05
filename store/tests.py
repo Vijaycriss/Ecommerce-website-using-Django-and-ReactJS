acd1a218965f0d0846c271e8ad16b974308887f2
Vijaycriss <vijaycriss123@gmail.com>
2024-04-05 16:51:10 +0530

    added test

--- store/tests.py
+++ store/tests.py
@@ -3,6 +3,7 @@ from .models import customers
 
 # Create your tests here.
 #test
+#1. test_customer_creation(self):
 class  CustomerModelTest(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
