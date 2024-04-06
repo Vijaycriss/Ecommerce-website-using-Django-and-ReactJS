03cb28d0d47b8832ad63836c25b1298ad7574c98
Vijaycriss <vijaycriss123@gmail.com>
2024-04-06 20:07:30 +0530

    database

--- store/models/customers.py
+++ store/models/customers.py
@@ -3,7 +3,7 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone_no = models.IntegerField(max_length=10, null=True, blank=True)
+    phone_no = models.IntegerField()
     email = models.EmailField()
     password = models.CharField(max_length=100)
     
